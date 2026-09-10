4907c4bce7db1d84a9c8555bc60b84c10f0ccef1
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2026-09-10 22:08:27 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map()
+
+        for (let str of strs){
+            const sortedStr = str.split('').sort().join('')
+
+            if(map.has(sortedStr)){
+                map.get(sortedStr).push(str)
+            } else{
+                map.set(sortedStr, [str])
+            }
+        }
+
+        return Array.from(map.values())
+
+    }
+}
